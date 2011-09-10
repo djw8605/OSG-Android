afbc42fdf4d9757acff45f74c337998a3329463e
Derek Weitzel <djw8605@gmail.com>
2011-09-10 16:31:19 -0500

    Fixed OSG Icon

--- gen/com/example/helloandroid/R.java
+++ gen/com/example/helloandroid/R.java
@@ -15,10 +15,16 @@ public final class R {
         public static final int osg_logo=0x7f020001;
     }
     public static final class id {
-        public static final int autoCompleteTextView1=0x7f050003;
-        public static final int confirm_site=0x7f050004;
-        public static final int graph_view=0x7f050005;
+        public static final int autoCompleteTextView1=0x7f05000a;
+        public static final int autoCompleteTextView2=0x7f050007;
+        public static final int confirm_site=0x7f05000b;
         public static final int imageView1=0x7f050000;
+        public static final int tableLayout1=0x7f050003;
+        public static final int tableRow1=0x7f050004;
+        public static final int tableRow2=0x7f050005;
+        public static final int tableRow3=0x7f050008;
+        public static final int textView1=0x7f050009;
+        public static final int textView2=0x7f050006;
         public static final int view_map=0x7f050002;
         public static final int view_monitoring=0x7f050001;
     }
@@ -29,10 +35,15 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int empty=0x7f04000a;
         public static final int enter_site=0x7f040004;
+        public static final int enter_vo=0x7f040008;
         public static final int get_usage=0x7f040005;
         public static final int hello=0x7f040000;
+        public static final int site=0x7f040009;
+        public static final int sites=0x7f040006;
         public static final int viewmap=0x7f040003;
         public static final int viewmonitoring=0x7f040002;
+        public static final int vos=0x7f040007;
     }
 }
