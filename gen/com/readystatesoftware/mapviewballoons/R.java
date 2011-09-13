13629b989df4afaf4914b58b42f614843d23fe5e
Derek Weitzel <djw8605@gmail.com>
2011-09-13 16:35:17 -0500

    Added landscape version of main page

--- gen/com/readystatesoftware/mapviewballoons/R.java
+++ gen/com/readystatesoftware/mapviewballoons/R.java
@@ -21,26 +21,28 @@ public final class R {
         public static final int osg_logo=0x7f020007;
     }
     public static final class id {
-        public static final int autoCompleteTextView1=0x7f050013;
+        public static final int autoCompleteTextView1=0x7f050015;
         public static final int balloon_inner_layout=0x7f050001;
         public static final int balloon_item_snippet=0x7f050003;
         public static final int balloon_item_title=0x7f050002;
         public static final int balloon_main_layout=0x7f050000;
         public static final int close_img_button=0x7f050004;
-        public static final int confirm_site=0x7f050014;
+        public static final int confirm_site=0x7f050016;
         public static final int imageView1=0x7f050005;
         public static final int linearLayout1=0x7f050006;
-        public static final int mainlayout=0x7f05000c;
-        public static final int mapview=0x7f05000d;
+        public static final int mainlayout=0x7f05000f;
+        public static final int mapview=0x7f050010;
         public static final int tableLayout1=0x7f050008;
+        public static final int tableLayout2=0x7f05000c;
         public static final int tableRow1=0x7f050009;
-        public static final int tableRow2=0x7f05000e;
-        public static final int tableRow3=0x7f050011;
-        public static final int textView1=0x7f050012;
-        public static final int textView2=0x7f05000f;
+        public static final int tableRow2=0x7f050011;
+        public static final int tableRow3=0x7f05000d;
+        public static final int tableRow4=0x7f05000e;
+        public static final int textView1=0x7f050014;
+        public static final int textView2=0x7f050012;
         public static final int view_map=0x7f05000b;
         public static final int view_monitoring=0x7f05000a;
-        public static final int vo_spinner=0x7f050010;
+        public static final int vo_spinner=0x7f050013;
         public static final int webView1=0x7f050007;
     }
     public static final class layout {
