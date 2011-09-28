84747c6eaea2676ae4e67af2fdb7e55309260929
Derek Weitzel <djw8605@gmail.com>
2011-09-28 11:43:52 -0500

    Removing unused imports.

--- src/com/osg/osgmon/monitoring/OSGSiteUsage.java
+++ src/com/osg/osgmon/monitoring/OSGSiteUsage.java
@@ -1,15 +1,11 @@
 package com.osg.osgmon.monitoring;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.Random;
 
